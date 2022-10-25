bc77e2a4847168d14e83d994a6f6f5040785ddcb
krezoiv <erick.adan@gmail.com>
2022-10-25 13:36:51 -0600

    25.10.1

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'mysql.cvzlccdnygep.us-east-1.rds.amazonaws.com',
-    user: 'adminseminario',
+    host: 'mysqlserver.cvzlccdnygep.us-east-1.rds.amazonaws.com',
+    user: 'seminario',
     password: 'Proyecto2022',
-    port: 3305,
+    port: 3306,
     database: 'seminariotics'
 });
 
